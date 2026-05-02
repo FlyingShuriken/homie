6bcc9cdc80fbdda77dea44ac9510e46b79bb105a
flyingshuriken <alvinchoi0623@gmail.com>
2026-05-02 11:53:05 +0800

    feat: add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "homie-backend",
+      cwd: "./backend",
+      interpreter: ".venv/bin/python",
+      script: "-m",
+      args: "uvicorn main:app --host 0.0.0.0 --port 8000",
+      watch: false,
+      autorestart: true,
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "homie-frontend",
+      cwd: "./frontend",
+      script: "node_modules/.bin/next",
+      args: "start --port 3000",
+      watch: false,
+      autorestart: true,
+    },
+  ],
+};
